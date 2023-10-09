0bd7506d184e4a7bfa417aa94fbf229576cd1ed7
Yanik Kumar <believemasters@gmail.com>
2023-10-09 12:49:44 +0530

    Changed text to upvote downvote

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,7 +1,7 @@
 <div class="text-center">
-        <Button class="btn btn-success rounded-0" wire:click="increment">+</Button>
+        <Button class="btn btn-success rounded-0" wire:click="increment">&#8679; Upvote</Button>
         <h3>{{ $counter }}</h3>
-        <Button class="btn btn-danger rounded-0" wire:click="decrement">-</Button>
+        <Button class="btn btn-danger rounded-0" wire:click="decrement">&#x21e9; Downvote</Button>
         <br />
-        <button class="btn btn-info rounded-0 mt-3" wire:click="resetCounter">Reset</button>
+        <button class="btn btn-info rounded-0 mt-3" wire:click="resetCounter">&#8597; Reset</button>
 </div>
